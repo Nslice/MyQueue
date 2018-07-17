507ee1e5e5ff61f643836bf3893e480f375dc3c5
Talgat <tairovtalgat@gmail.com>
2018-07-17 14:40:39 +0600

    nop

--- main.cpp
+++ main.cpp
@@ -1,58 +1,22 @@
 #include <iostream>
 #include "queue.h"
-#include "workermi.h"
 
-const int SIZE = 5;
 
 int main()
 {
-	Worker* lolas[SIZE];
-	Queue<Worker*> qu;
-	Worker* tmpWk;
 
-	int ct;
-	for (ct = 0; ct < SIZE; ct++)
+	Queue<int> q;
+
+	for (int i = 0; i < 10; i++)
+		q.push(rand() % 100);
+
+
+	while (!q.empty())
 	{
-		char choice;
-		cout << "Enter the employee category:\n"
-			<< "w: waiter  s: singer  "
-			<< "t: singing waiter  q: quit\n";
-		cin >> choice;
-		while (strchr("wstq", choice) == nullptr)
-		{
-			cout << "Please enter a w, s, t, or q: ";
-			cin >> choice;
-		}
-		if (choice == 'q')
-			break;
-
-		switch (choice)
-		{
-		case 'w':
-			qu.push(new Waiter);
-			break;
-		case 's':
-			qu.push(new Singer);
-			break;
-		case 't':
-			qu.push(new SingingWaiter);
-			break;
-		}
-		cin.get();
-		qu.back()->Set();
+		std::cout << q.front() << std::endl;
+		q.pop();
 	}
 
-	cout << "\nHere is your staff:\n";
-	int i;
-	for (i = 0; i < ct; i++)
-	{
-		cout << endl;
-		qu.front()->Show();
-		delete qu.front();
-		qu.pop();
-	}
-	
-	cout << "Bye.\n";
+
 	return 0;
 }
-
